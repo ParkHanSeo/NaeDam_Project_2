2b9f2d44d9c62c628277572ade62ba48987ee32e
parkjeehoon <gkstj8300@naver.com>
2022-06-04 00:02:56 +0900

    박지훈 : 개선 진행중 06/04 12:02

--- src/main/java/com/naedam/admin/community/model/dao/CommunityDao.java
+++ src/main/java/com/naedam/admin/community/model/dao/CommunityDao.java
@@ -14,47 +14,50 @@ import com.naedam.admin.community.model.vo.SmsSetting;
 
 public interface CommunityDao {
 
-	// review
-	List<Review> reviewList(Map<Object, Object> param);
-
+	// review				: 후기 목록 조회
+	int totalReviewCount(Map<String, Object> param);
+	List<Object> reviewList(Map<String, Object> param);
+	
+	// reviewModal			: 후기 상세보기
 	List<Review> reviewModal(int reviewCode);
-
 	List<ReviewImg> reviewImgModal(int reviewCode);
 
+	// delete				: 후기 삭제
 	int selectCategory(String reviewCode);	
-	
 	int imgDelete(String reviewCode);	
-	
 	int selectDelete(String reviewCode);
-	
-	int totalReviewCount(Map<Object, Object> param);
 
-	// sms
-	MsgInfo selectMsgInfo(long orderNo);
+	// smsList				: SMS 목록 조회
+	int totalSmsCount(Map<String, Object> param);
+	List<Object> selectSmsList(Map<String, Object> param);
 	
-	List<SmsSetting> smsCheck(String templateId);	
-	
-	int smsAutoSend(HashMap<String, String> param);
-	
-	int insertSms(HashMap<String, Object> param2);
-	
-	List<SmsSetting> selectSmsSetting();
-	List<Sms> selectSmsList(HashMap<String, Object> param);
-	
+	// charge				: SMS 이용 요금 조회
 	int countBySendPhone(String phone);
 	
-	int totalSmsCount(HashMap<String, Object> param);
+	// smsAutoSendCheck		: SMS 자동발송 여부 조회
+	List<Object> selectSmsSetting();
 	
-	// email
-	List<EmailSetting> emailCheck(String templateId);
+	// smsSettingModify		: SMS 설정 저장
+	int smsAutoSend(Map<String, Object> setMod);
 	
-	int mailAutoSend(HashMap<String, String> param);
+	// emailList			: Email 목록 조회
+	int totalEmailCount(Map<String, Object> param);
+	List<Object> selectEmailList(Map<String, Object> param);
+	
+	// emailAutoSendCheck	: Email 자동발송 여부 조회
+	List<Object> selectEmailSetting();
+	
+	// emailSettingModify	: Email 설정 저장 
+	int emailAutoSend(Map<String, Object> setMod);
 
+	// 회원 정보 조회
+	MsgInfo selectMsgInfo(long orderNo);
+	
+	// 설정값 조회
+	List<SmsSetting> smsAutoChecked(String templateId);
+	List<EmailSetting> emailAutoChecked(String templateId);
+	
+	// 발송 목록 추가
+	int insertSms(HashMap<String, Object> param2);
 	int insertEmail(HashMap<String, Object> param2);
-
-	List<EmailSetting> selectEmailSetting();
-	List<Email> selectEmailList(HashMap<String, Object> param);
-
-	int totalEmailCount(HashMap<String, Object> param);
-
 }
